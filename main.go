88ada079d040fdfca836aea6f14f6dc625ccc795
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-12-21 02:16:27 +0900

    Add C wrapper

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 // #cgo LDFLAGS: -lncurses
 // #include<ncurses.h>
+// #include"main.h"
 import "C"
 
 
 func main() {
     C.initscr()
-    C.getch()
+
+    row := 12
+    col := 30
+    message := "Hello, World!"
+    C.myMvprintw(C.int(row), C.int(col), C.CString(message))
+
+    for {
+        ch := C.getch()
+        if ch == C.int('q') {
+            break
+        }
+    }
+
     C.endwin()
 }
