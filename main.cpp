8a27d9a9cf6cfa2a37823da3869ed9dda0786c36
takayasumorishita <c011729632@edu.teu.ac.jp>
2021-01-01 23:44:12 +0900

    Add functions (setup, update, draw)

--- main.cpp
+++ main.cpp
@@ -3,10 +3,28 @@
 #include <random>
 #include <future>
 
+MEVENT me;
+int px = 40;
+const int P_Y = 23;
+bool is_playing, has_ball;
+double bx, by, vx, vy;
+
+void setup();
+void update();
+void draw();
 
 int main(void)
 {
     initscr();
+    setup();
+    while (true) {
+        update();
+        draw();
+    }
+    endwin();
+}
+
+void setup() {
     // NO SHOW INPUT KEY
     noecho();
     // NO SHOW INPUT KEY
@@ -15,26 +33,23 @@ int main(void)
     keypad(stdscr, TRUE);
     // GET MOUSE EVENT (XTERM)
     mousemask(ALL_MOUSE_EVENTS, NULL);
+}
 
-    MEVENT me;
-    int px = 2;
-
-    while (true) {
-        int ch = getch();
-        if (ch == 'q') break;
-        if (ch == KEY_MOUSE) {
-            if (getmouse(&me) == OK) {
-                clear();
-                px = me.x;
-                if (px < 2)px = 2;
-                if (px > 77)px = 77;
-                mvprintw(23, px - 2, "-----");
-                refresh();
-            }
+void update() {
+    int ch = getch();
+    if (ch == 'q') break;
+    if (ch == KEY_MOUSE) {
+        if (getmouse(&me) == OK) {
+            clear();
+            px = me.x;
+            if (px < 2)px = 2;
+            if (px > 77)px = 77;
+            mvprintw(23, px - 2, "-----");
+            refresh();
         }
     }
-    endwin();
 }
 
-void setup () {
+void draw() {
 }
+
